dce8c7b0d6aa539e547fc89b6ccee1f944e1568e
Lukas <67557737+kalvelukas@users.noreply.github.com>
2020-12-04 14:59:09 +0100

    worked on files

--- tests/test_dice.py
+++ tests/test_dice.py
@@ -5,8 +5,16 @@ from code import dice
 from code.dice import RollStandard
 
 class TestRollStandard(unittest.TestCase):
+    @classmethod
+    def setUpClass(cls):
+        pass
+
+    @classmethod
+    def tearDownClass(cls):
+        pass
+
     def setUp(self):
-        roll1 = RollStandard
+        self.roll1 = RollStandard()
         self.numblist = list(range(1, 100, 5))
         self.pipslist = list(range(1, 10001, 100))
         self.frinlist = list(range(101, 10001, 100))
@@ -14,43 +22,43 @@ class TestRollStandard(unittest.TestCase):
         self.negalist = list(range(-104, 0, 5))
         self.floalist = [0.1, 12.1, 0.92851, 97872.742, 0.05]
         self.charlist = ["a","z", "b", "eface", "<@@////"]
-        pass 
+        pass
 
     def tearDown(self):
         pass
 
-    def test_roll(self):
-        self.assertEqual((len(roll1.roll()) == 1), True)
-        self.assertEqual((max(roll1.roll()) <= 6), True)
-        for numb in self.numblist:    
-            self.assertEqual((min(roll1.roll(numb, 100)) >= 1), True)
-            self.assertEqual((max(roll1.roll(numb)) <= 6), True)
-            self.assertEqual((len(RollStandard.roll(numb)) == numb), True)
-            self.assertEqual((len(RollStandard.roll(numb, numb)) == numb), True)
-            self.assertEqual((max(RollStandard.roll(numb, numb)) <= numb), True)
+    def test_roll_standard(self):
+        self.assertEqual((len(self.roll1.roll()) == 1), True)
+        self.assertEqual((max(self.roll1.roll()) <= 6), True)
+        for numb in self.numblist:
+            self.assertEqual((min(self.roll1.roll(numb, 100)) >= 1), True)
+            self.assertEqual((max(self.roll1.roll(numb)) <= 6), True)
+            self.assertEqual((len(self.roll1.roll(numb)) == numb), True)
+            self.assertEqual((len(self.roll1.roll(numb, numb)) == numb), True)
+            self.assertEqual((max(self.roll1.roll(numb, numb)) <= numb), True)
             for pips in self.pipslist:
-                self.assertEqual((max(RollStandard.roll(numb, pips)) <= pips), True)
-                self.assertEqual((len(RollStandard.roll(numb, pips)) == numb), True)
+                self.assertEqual((max(self.roll1.roll(numb, pips)) <= pips), True)
+                self.assertEqual((len(self.roll1.roll(numb, pips)) == numb), True)
         # for char in self.charlist:
-        #     self.assertRaises(TypeError, RollStandard.roll, char)
-        #     self.assertRaises(TypeError, RollStandard.roll, char, char)
+        #     self.assertRaises(TypeError, self.roll1.roll, char)
+        #     self.assertRaises(TypeError, self.roll1.roll, char, char)
         #     for pips in self.pipslist:
-        #         self.assertRaises(TypeError, RollStandard.roll, char, pips)
-        #         self.assertRaises(TypeError, RollStandard.roll, pips, char)
+        #         self.assertRaises(TypeError, self.roll1.roll, char, pips)
+        #         self.assertRaises(TypeError, self.roll1.roll, pips, char)
         #     for floa in self.floalist:
-        #         self.assertRaises(TypeError, RollStandard.roll, char, floa)
-        #         self.assertRaises(TypeError, RollStandard.roll, floa, floa)
+        #         self.assertRaises(TypeError, self.roll1.roll, char, floa)
+        #         self.assertRaises(TypeError, self.roll1.roll, floa, floa)
         # for nega in self.negalist:
-        #     self.assertRaises(ValueError, RollStandard.roll, nega)
-        #     self.assertRaises(ValueError, RollStandard.roll, nega, nega)
+        #     self.assertRaises(ValueError, self.roll1.roll, nega)
+        #     self.assertRaises(ValueError, self.roll1.roll, nega, nega)
         #     for pips in self.pipslist:
-        #         self.assertRaises(ValueError, RollStandard.roll, nega, pips)
+        #         self.assertRaises(ValueError, self.roll1.roll, nega, pips)
         # for entry in self.frinlist:
-        #     self.assertRaises(NotImplementedError, RollStandard.roll, entry)
-        #     self.assertRaises(NotImplementedError, RollStandard.roll, entry, entry)
+        #     self.assertRaises(NotImplementedError, self.roll1.roll, entry)
+        #     self.assertRaises(NotImplementedError, self.roll1.roll, entry, entry)
 
-    def test_modify(self):
+    def test_roll_modified(self):
         pass
 
-    def test_reroll(self):
+    def test_roll_rerolls(self):
         pass
